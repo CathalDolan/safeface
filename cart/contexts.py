3a43c83a432d7176f1cc99612ec130b11d8ebfda
CathalDolan <cathal@gizagig.com>
2021-03-14 15:13:00 +0000

    Quantity price breaks added to models and min qty updated on Product Info

--- cart/contexts.py
+++ cart/contexts.py
@@ -8,6 +8,7 @@ def cart_contents(request):
 
     cart_items = []
     indiv_product_total = []
+    price2 = 0
     product_total = 0
     product_count = 0
     products_total = 0
@@ -15,11 +16,26 @@ def cart_contents(request):
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
+        
+        if quantity >= product.qty_1:
+            price2 = product.price_1
+            print("price 1")
+        elif quantity >= product.qty_2:
+            price2 = product.price_2
+            print("price 2")
+        elif quantity >= product.qty_3:
+            price2 = product.price_3
+            print("price 3")
+        elif quantity >= product.qty_4:
+            price2 = product.price_4
+            print("price 4")
+        elif quantity >= product.qty_5:
+            price2 = product.price_5
+            print("price 5")
+
         product_total = quantity * product.price
-        print('product', product_total)
         product_count += quantity
         products_total += product_total
-        print('products', products_total)
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
--- products/models.py
+++ products/models.py
@@ -34,6 +34,16 @@ class Product(models.Model):
     standard = models.CharField(max_length=254, null=True, blank=True)
     dimensions = models.CharField(max_length=254, null=True, blank=True)
     material = models.CharField(max_length=254, null=True, blank=True)
+    qty_1 = models.IntegerField(default=50)
+    price_1 = models.DecimalField(max_digits=6, decimal_places=2, default=1)
+    qty_2 = models.IntegerField(default=100)
+    price_2 = models.DecimalField(max_digits=6, decimal_places=2, default=0.9)
+    qty_3 = models.IntegerField(default=250)
+    price_3 = models.DecimalField(max_digits=6, decimal_places=2, default=0.8)
+    qty_4 = models.IntegerField(default=500)
+    price_4 = models.DecimalField(max_digits=6, decimal_places=2, default=0.7)
+    qty_5 = models.IntegerField(default=1000)
+    price_5 = models.DecimalField(max_digits=6, decimal_places=2, default=0.6)
 
     def __str__(self):
         return self.name
