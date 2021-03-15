441ccd1ffc8f7ca3c9d5d3f772a5fb028e7c59c2
CathalDolan <cathal@gizagig.com>
2021-03-15 20:16:29 +0000

    Toasts added with success content updated to match cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -19,6 +19,7 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.name} to your cart')
@@ -31,13 +32,16 @@ def add_to_cart(request, item_id):
 # Update the cart Qty from the Cart page
 def update_cart(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
     print(quantity)
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(request, f'{product.name} removed from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -48,11 +52,13 @@ def delete_from_cart(request, item_id):
 
     try:
         cart = request.session.get('cart', {})
-
+        product = get_object_or_404(Product, pk=item_id)
         cart.pop(item_id)
+        messages.success(request, f'{product.name} removed from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing it: {e}')
         return HttpResponse(status=500)
