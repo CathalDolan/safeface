78b964e51547c62f74e4c799b2e3621f8752f6d8
CathalDolan <cathal@gizagig.com>
2021-03-15 20:59:30 +0000

    Checkout App and its models created

---
+++ checkout/models.py
@@ -0,0 +1,73 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
+
+from products.models import Product
+
+
+# This is the overall order
+class Order(models.Model):
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    company_name = models.CharField(max_length=254, null=False, blank=False)
+    department = models.CharField(max_length=254, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    products_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    net_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    vat = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    gross_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+
+        self.products_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.products_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.products_total + settings.STANDARD_DELIVERY_PRICE
+        else:
+            self.delivery_cost = 0
+        self.net_total = self.products_total + self.delivery_cost
+        self.vat = self.net_total * settings.VAT_RATE
+        self.gross_total = self.net_total + self.vat
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+# Each individual product is given it's own line on the order form with a total cost for that line
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_colour = models.CharField(max_length=50, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
