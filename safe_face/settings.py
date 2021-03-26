5d8ba20b73dfd85e24c63ce3cacb8dae16aebc87
CathalDolan <cathal@gizagig.com>
2021-03-26 15:17:16 +0000

    Settings Update

--- safe_face/settings.py
+++ safe_face/settings.py
@@ -186,7 +186,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Configuration
-    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'safe-face'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
