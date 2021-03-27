fd7c30994d23a439a3b99897a9b2c1ff15576e7c
CathalDolan <cathal@gizagig.com>
2021-03-27 16:37:07 +0000

    Cart: Fixed bug to allow Update button work. Also added display functionality

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import NewsletterUser
+from .forms import NewsletterUserSignUpForm
+
+def newsletter_signup(request):
+    form = NewsletterUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        # Check to see if email address already exists
+        if NewsletterUser.objects.filter(email=instance.email).exists():
+            print("Sorry! This email exists")
+        else:
+            instance.save()
+
+    context = {
+        'form': form,
+    }
+    template = "newsletter/sign_up.html"
+    return render(request, template, context)
+
+
+def newsletter_unsubscribe(request):
+    form = NewsletterUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        # Check to see if email address already exists
+        if NewsletterUser.objects.filter(email=instance.email).exists():
+            print("Sorry! This email exists")
+        else:
+            instance.save()
+
+    context = {
+        'form': form,
+    }
+    template = "newsletter/sign_up.html"
+    return render(request, template, context)
